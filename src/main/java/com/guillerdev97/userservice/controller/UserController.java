f7f5736789927547b75f1dedb7bb5f5d46e71a01
Guillermo García <guillermieres97@gmail.com>
2022-11-14 12:12:17 +0100

    define all methods of the user controller

--- src/main/java/com/guillerdev97/userservice/controller/UserController.java
+++ src/main/java/com/guillerdev97/userservice/controller/UserController.java
@@ -1,13 +1,15 @@
 package com.guillerdev97.userservice.controller;
 
+import com.guillerdev97.userservice.domain.AppRole;
 import com.guillerdev97.userservice.domain.AppUser;
 import com.guillerdev97.userservice.service.UserService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,5 +21,31 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<AppUser>> getUsers() {
         return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+    @PostMapping
+    public ResponseEntity<AppUser> saveUser(@RequestBody AppUser user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/v1/users/").toUriString());
+
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<AppRole> saveRole(@RequestBody AppRole role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/v1/users/").toUriString());
+
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    public ResponseEntity<?> addAppRoleToAppUser(@RequestBody RoleToUserForm form) {
+        userService.addAppRoleToAppUser(form.getUsername(), form.getRolename());
+
+        return ResponseEntity.ok().build();
+    }
+
+    @Data
+    class RoleToUserForm {
+        private String username;
+        private String rolename;
     }
 }
