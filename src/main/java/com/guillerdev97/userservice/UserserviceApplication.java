1c33c170e7cc6c4278d6e2dff4a4e44c1e633c9a
Guillermo García <guillermieres97@gmail.com>
2022-11-14 17:06:28 +0100

    start implement security configuration

--- src/main/java/com/guillerdev97/userservice/UserserviceApplication.java
+++ src/main/java/com/guillerdev97/userservice/UserserviceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.management.relation.Role;
 import java.util.ArrayList;
@@ -17,6 +19,11 @@ public class UserserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserserviceApplication.class, args);
     }
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Bean
     CommandLineRunner run(UserService userService) {
---
+++ src/main/java/com/guillerdev97/userservice/security/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.guillerdev97.userservice.security;
+
+import com.guillerdev97.userservice.service.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+}
