26c6b97a3cb0a41e2aaef3cdd5f36222fbb243dc
Guillermo García <guillermieres97@gmail.com>
2022-11-15 10:48:06 +0100

    remove security config and change controller route

--- src/main/java/com/guillerdev97/userservice/security/SecurityConfig.java
+++ src/main/java/com/guillerdev97/userservice/security/SecurityConfig.java
@@ -1,16 +1,12 @@
 package com.guillerdev97.userservice.security;
 
-import com.guillerdev97.userservice.service.UserServiceImpl;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
+@Slf4j
 public class SecurityConfig {
 
 }
